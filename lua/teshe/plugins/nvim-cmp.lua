e14ecfd6b3481f72bc29bf2e009d34076ee64c8b
teshe-123 <teshomeabebe234@gmail.com>
2023-12-13 19:03:20 +0300

    Added <CR> for autocompletion confirmation

--- lua/teshe/plugins/nvim-cmp.lua
+++ lua/teshe/plugins/nvim-cmp.lua
@@ -37,7 +37,7 @@ end
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
         ["<C-e>"] = cmp.mapping.abort(), -- close completion window
-        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
     ["<Tab>"] = cmp.mapping(function(fallback)
       -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable() 
       -- that way you will only jump inside the snippet region
