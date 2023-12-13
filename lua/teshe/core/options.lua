2cf8c31c614243215a15c9b99ace210422366383
teshe-123 <teshomeabebe234@gmail.com>
2023-12-13 19:30:22 +0300

    tweaking neovim options

--- lua/teshe/core/options.lua
+++ lua/teshe/core/options.lua
@@ -30,6 +30,14 @@ opt.termguicolors = true
 opt.background = "dark" -- colorschemes that can be light or dark will be made dark
 opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 
+-- swap files
+vim.opt.swapfile = false
+vim.opt.undodir = os.getenv("HOME") .. "/.nvim/undodir"
+vim.opt.undofile = true
+vim.opt.hlsearch = false
+vim.opt.updatetime = 50
+
+
 -- backspace
 opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
 
